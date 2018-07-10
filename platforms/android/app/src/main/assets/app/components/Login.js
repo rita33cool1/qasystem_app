030d5f9c4d386be2a337a9b69390d96b8e567dd5
penguin210135 <john98562019@gmail.com>
2018-07-10 22:42:03 +0800

    add login secure

--- platforms/android/app/src/main/assets/app/components/Login.js
+++ platforms/android/app/src/main/assets/app/components/Login.js
@@ -69,7 +69,7 @@ module.exports = {
         <StackLayout>
             <Label :text="Login" />
             <TextField v-model="AccountText" hint="Account..." />
-            <TextField v-model="PasswordText" hint="Password..." />
+            <TextField v-model="PasswordText" hint="Password..." secure=true/>
             <Button text="Submit" @tap="signin()" />
             <Button text="Back" @tap="$router.go(-1)" />
         </StackLayout>
