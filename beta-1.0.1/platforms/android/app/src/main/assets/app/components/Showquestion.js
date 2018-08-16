b1f361c565d649d281083f4f9392269c80a3084a
unknown <john98562019@gmail.com>
2018-08-17 00:44:41 +0800

    add comment function

--- beta-1.0.1/platforms/android/app/src/main/assets/app/components/Showquestion.js
+++ beta-1.0.1/platforms/android/app/src/main/assets/app/components/Showquestion.js
@@ -4,8 +4,8 @@ module.exports = {
         return {
             apiUrl: "http://140.114.79.86:8000/api/question/?qid=" + this.$cur_qid.val,
             deleteUrl: "http://140.114.79.86:8000/api/user/question/delete/",
-            deleteAnswerUrl:  "http://140.114.79.86:8000/api/question/answer/delete/",
-            deleteCommentUrl:  "http://140.114.79.86:8000/api/question/comment/delete/",
+            deleteAnswerUrl: "http://140.114.79.86:8000/api/question/answer/delete/",
+            deleteCommentUrl: "http://140.114.79.86:8000/api/question/comment/delete/",
             title: "Tmp title",
             content: "None",
             askername: "None",
@@ -15,7 +15,7 @@ module.exports = {
         }
     },
     methods: {
-        load: function() {
+        load: function () {
             console.log("Show the question");
             httpModule.request({
                 url: this.apiUrl,
@@ -23,8 +23,8 @@ module.exports = {
                 headers: { "Content-Type": "application/json" }
             }).then((response) => {
                 const result = response.content.toJSON();
-                console.log(result.question[0]); 
-                
+                console.log(result.question[0]);
+
                 this.title = result.question[0].title;
                 this.content = result.question[0].content;
                 this.askername = result.question[0].user;
@@ -35,20 +35,31 @@ module.exports = {
                 }
 
                 //comments
-                /*
+
                 for (var i = 0; i < result.question[0].comments.length; i++) {
-                    this.expertises.push(result.question[0].expertises[i]);
+                    var own_user = false;
+                    if (this.$user_name.val == result.question[0].comments[i].user) {
+                        own_user = true;
+                    }
+
+                    var tmp = {
+                        answer_id: result.question[0].comments[i].id,
+                        content: result.question[0].comments[i].content,
+                        user: result.question[0].comments[i].user,
+                        own: own_user
+                    };
+                    this.comments.push(tmp);
                 }
-                */
+
                 //answers
                 for (var i = 0; i < result.answers.length; i++) {
 
                     var own_user = false;
-                    if(this.$user_name.val == result.answers[i].user){
+                    if (this.$user_name.val == result.answers[i].user) {
                         own_user = true;
                     }
 
-                    var tmp={
+                    var tmp = {
                         answer_id: result.answers[i].id,
                         content: result.answers[i].content,
                         user: result.answers[i].user,
@@ -56,13 +67,11 @@ module.exports = {
                     };
                     this.answers.push(tmp);
                 }
-                console.log(this.answers);
-                
             }, (e) => {
                 console.log(e);
             });
         },
-        deleteQuestion: function() {
+        deleteQuestion: function () {
             console.log("delete the question");
             httpModule.request({
                 url: this.deleteUrl,
@@ -82,19 +91,25 @@ module.exports = {
                 console.log(e);
             });
         },
-        modifyQuestion: function() {
+        modifyQuestion: function () {
             console.log("modify the question");
             this.$router.push('/modifyquestion');
         },
-        responseQuestion: function() {
+        responseQuestion: function () {
             console.log("response the question");
             this.$router.push('/answer');
         },
-        modifyAnswer: function() {
+        sendcomment: function (type,id) {
+            this.$comment_type.val = type;
+            this.$cur_answer_id.val = id;
+            console.log("send the comment");
+            this.$router.push('/sendcomment');
+        },
+        modifyAnswer: function () {
             console.log("modify the answer");
             //this.$router.push('/modifyquestion');
         },
-        deleteAnswer: function(id) {
+        deleteAnswer: function (id) {
             httpModule.request({
                 url: this.deleteAnswerUrl,
                 method: "POST",
@@ -139,7 +154,34 @@ module.exports = {
                 </FormattedString>
             </TextView>
 
-            <ListView class="list-group" for="answer in answers" style="height:300px width:60px">
+            <FlexboxLayout flexDirection="row" class="list-group-item">
+                <Button text="delete" v-if="this.$user_name.val == this.askername" @tap="deleteQuestion()" />
+                <Button text="modify" v-if="this.$user_name.val == this.askername" @tap="modifyQuestion()" />
+                <Button text="Response" v-if="this.$user_name.val != this.askername && this.$user_id.val != '0' " @tap="responseQuestion()" />
+                <Button text="Comment" v-if="this.$user_name.val != this.askername && this.$user_id.val != '0' " @tap="sendcomment('question' , 0)" />
+            </FlexboxLayout>
+
+            <Label text="Comment:" />
+            <ListView class="list-group" for="comment in comments" height="100" >
+                <v-template>
+                     <FlexboxLayout flexDirection="column" class="list-group-item">
+                        <TextView editable="false">
+                            <FormattedString> 
+                            <Span :text="comment.user" />
+                            <Span text=":\n" />
+                            <Span :text="comment.content" />
+                            </FormattedString>
+                        </TextView>
+                        <FlexboxLayout flexDirection="row" class="list-group-item">
+                            <Button text="Edit" v-if="comment.own == true" @tap="" />
+                            <Button text="Delete" v-if="comment.own == true" @tap="" />
+                        </FlexboxLayout>
+                    </FlexboxLayout>
+                </v-template>
+            </ListView>
+
+            <Label text="Answer:" />
+            <ListView class="list-group" for="answer in answers" height="250" >
                 <v-template>
                     <FlexboxLayout flexDirection="column" class="list-group-item">
                         <TextView editable="false">
@@ -152,14 +194,13 @@ module.exports = {
                         <FlexboxLayout flexDirection="row" class="list-group-item">
                             <Button text="Edit" v-if="answer.own == true" @tap="modifyAnswer()" />
                             <Button text="Delete" v-if="answer.own == true" @tap="deleteAnswer(answer.answer_id)" />
+                            <Button text="Comment" v-if="answer.own != true" @tap="sendcomment('answer', answer.answer_id)" />
                         </FlexboxLayout>
                     </FlexboxLayout>
                 </v-template>
             </ListView>
 
-            <Button text="delete" v-if="this.$user_name.val == this.askername" @tap="deleteQuestion()" />
-            <Button text="Response" v-if="this.$user_name.val != this.askername && this.$user_id.val != '0' " @tap="responseQuestion()" />
-            <Button text="modify" v-if="this.$user_name.val == this.askername" @tap="modifyQuestion()" />
+            
         </StackLayout>
     </Page>
   `
