dc1cdfd47842c93d7277c5dc51d13ce42bc7a210
unknown <john98562019@gmail.com>
2018-08-18 13:08:50 +0800

    update package.json

--- beta-1.0.1/platforms/android/app/build/intermediates/assets/debug/app/components/Showquestion.js
+++ beta-1.0.1/platforms/android/app/build/intermediates/assets/debug/app/components/Showquestion.js
@@ -4,15 +4,18 @@ module.exports = {
         return {
             apiUrl: "http://140.114.79.86:8000/api/question/?qid=" + this.$cur_qid.val,
             deleteUrl: "http://140.114.79.86:8000/api/user/question/delete/",
+            deleteAnswerUrl: "http://140.114.79.86:8000/api/question/answer/delete/",
+            deleteCommentUrl: "http://140.114.79.86:8000/api/question/comment/delete/",
             title: "Tmp title",
             content: "None",
             askername: "None",
             expertises: [],
             answers: [],
+            comments: [],
         }
     },
     methods: {
-        load: function() {
+        load: function () {
             console.log("Show the question");
             httpModule.request({
                 url: this.apiUrl,
@@ -20,25 +23,55 @@ module.exports = {
                 headers: { "Content-Type": "application/json" }
             }).then((response) => {
                 const result = response.content.toJSON();
-                console.log(result); //result[0]:question result[1]:answers
-                
-                this.title = result[0].title;
-                this.content = result[0].content;
-                this.askername = result[0].username;
+                console.log(result.question[0]);
 
-                for (var i = 0; i < result[0].expertises.length; i++) {
-                    this.expertises.push(result[0].expertises[i]);
+                this.title = result.question[0].title;
+                this.content = result.question[0].content;
+                this.askername = result.question[0].user;
+
+                //expertises
+                for (var i = 0; i < result.question[0].expertises.length; i++) {
+                    this.expertises.push(result.question[0].expertises[i]);
                 }
-                    
-                for (var i = 0; i < result[0].answer_number; i++) {
-                    this.answers.push(result[1][i].content);
+
+                //comments
+
+                for (var i = 0; i < result.question[0].comments.length; i++) {
+                    var own_user = false;
+                    if (this.$user_name.val == result.question[0].comments[i].user) {
+                        own_user = true;
+                    }
+
+                    var tmp = {
+                        answer_id: result.question[0].comments[i].id,
+                        content: result.question[0].comments[i].content,
+                        user: result.question[0].comments[i].user,
+                        own: own_user
+                    };
+                    this.comments.push(tmp);
                 }
-                console.log(this.answers);
+
+                //answers
+                for (var i = 0; i < result.answers.length; i++) {
+
+                    var own_user = false;
+                    if (this.$user_name.val == result.answers[i].user) {
+                        own_user = true;
+                    }
+
+                    var tmp = {
+                        answer_id: result.answers[i].id,
+                        content: result.answers[i].content,
+                        user: result.answers[i].user,
+                        own: own_user
+                    };
+                    this.answers.push(tmp);
+                }
             }, (e) => {
                 console.log(e);
             });
         },
-        deleteQuestion: function() {
+        deleteQuestion: function () {
             console.log("delete the question");
             httpModule.request({
                 url: this.deleteUrl,
@@ -58,14 +91,45 @@ module.exports = {
                 console.log(e);
             });
         },
-        modifyQuestion: function() {
+        modifyQuestion: function () {
             console.log("modify the question");
             this.$router.push('/modifyquestion');
         },
-        responseQuestion: function() {
+        responseQuestion: function () {
             console.log("response the question");
             this.$router.push('/answer');
-        }
+        },
+        sendcomment: function (type,id) {
+            this.$comment_type.val = type;
+            this.$cur_answer_id.val = id;
+            console.log("send the comment");
+            this.$router.push('/sendcomment');
+        },
+        modifyAnswer: function () {
+            console.log("modify the answer");
+            //this.$router.push('/modifyquestion');
+        },
+        deleteAnswer: function (id) {
+            httpModule.request({
+                url: this.deleteAnswerUrl,
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                content: JSON.stringify({
+                    key: this.$user_id.val,
+                    answer_id: id
+                })
+            }).then((response) => {
+                const result = response.content.toJSON();
+                console.log(result);
+                if (result.msg == "Success") {
+                    alert(result.msg);
+                    this.$router.go(-1);
+                }
+            }, (e) => {
+                console.log(e);
+            });
+        },
+
     },
     template: `
     <Page @loaded="load()" >
@@ -89,9 +153,54 @@ module.exports = {
                     <Span text="\n" />
                 </FormattedString>
             </TextView>
-            <Button text="delete" v-if="this.$user_name.val == this.askername" @tap="deleteQuestion()" />
-            <Button text="Response" v-if="this.$user_name.val != this.askername && this.$user_id.val != '0' " @tap="responseQuestion()" />
-            <Button text="modify" v-if="this.$user_name.val == this.askername" @tap="modifyQuestion()" />
+
+            <FlexboxLayout flexDirection="row" class="list-group-item">
+                <Button text="delete" v-if="this.$user_name.val == this.askername" @tap="deleteQuestion()" />
+                <Button text="modify" v-if="this.$user_name.val == this.askername" @tap="modifyQuestion()" />
+                <Button text="Response" v-if="this.$user_name.val != this.askername && this.$user_id.val != '0' " @tap="responseQuestion()" />
+                <Button text="Comment" v-if="this.$user_name.val != this.askername && this.$user_id.val != '0' " @tap="sendcomment('question' , 0)" />
+            </FlexboxLayout>
+
+            <Label text="Comment:" />
+            <ListView class="list-group" for="comment in comments"  >
+                <v-template>
+                     <FlexboxLayout flexDirection="column" class="list-group-item">
+                        <TextView editable="false">
+                            <FormattedString> 
+                            <Span :text="comment.user" />
+                            <Span text=":\n" />
+                            <Span :text="comment.content" />
+                            </FormattedString>
+                        </TextView>
+                        <FlexboxLayout flexDirection="row" class="list-group-item">
+                            <Button text="Edit" v-if="comment.own == true" @tap="" />
+                            <Button text="Delete" v-if="comment.own == true" @tap="" />
+                        </FlexboxLayout>
+                    </FlexboxLayout>
+                </v-template>
+            </ListView>
+
+            <Label text="Answer:" />
+            <ListView class="list-group" for="answer in answers" >
+                <v-template>
+                    <FlexboxLayout flexDirection="column" class="list-group-item">
+                        <TextView editable="false">
+                            <FormattedString> 
+                            <Span :text="answer.user" />
+                            <Span text=":\n" />
+                            <Span :text="answer.content" />
+                            </FormattedString>
+                        </TextView>
+                        <FlexboxLayout flexDirection="row" class="list-group-item">
+                            <Button text="Edit" v-if="answer.own == true" @tap="modifyAnswer()" />
+                            <Button text="Delete" v-if="answer.own == true" @tap="deleteAnswer(answer.answer_id)" />
+                            <Button text="Comment" v-if="answer.own != true" @tap="sendcomment('answer', answer.answer_id)" />
+                        </FlexboxLayout>
+                    </FlexboxLayout>
+                </v-template>
+            </ListView>
+
+            
         </StackLayout>
     </Page>
   `
